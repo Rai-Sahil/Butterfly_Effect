dac60dc9a1a79a78aa2933a8a5ae11e3f7c0195b
DelsonTan <delsonwtan@gmail.com>
2022-04-26 16:04:07 -0700

    Add login page html and css files, include express session and update routes in server to include login page.

--- index.js
+++ index.js
@@ -1,15 +1,37 @@
 const fs = require("fs");
 const express = require('express');
+const session = require("express-session");
 
 const app = express();
+const port = 8000;
 
-app.use('/js', express.static('./public/js'));
-app.use('/css', express.static('./public/css'));
-app.use('/img', express.static('./public/img'));
+app.use(express.static(__dirname + "/public"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-app.get('/', function (req, res) {
-    let doc = fs.readFileSync('./public/html/index.html', "utf8");
-    res.send(doc);
+app.use(
+  session({
+    name: "teamname.sid",
+    resave: false,
+    saveUninitialized: true,
+    secret: "bby32",
+  })
+);
+
+app.get("/", function (req, res) {
+  if (req.session.loggedIn) {
+    res.sendFile("index.html", { root: __dirname + "/public/html" });
+  } else {
+    res.redirect("/login");
+  }
+});
+
+app.get("/login", function (req, res) {
+  if (req.session.loggedIn) {
+    res.redirect("/");
+  } else {
+    res.sendFile("login.html", { root: __dirname + "/public/html" });
+  }
 });
 
 app.get('/data/newsfeed', function (req, res) {
@@ -21,5 +43,4 @@ app.use(function (req, res, next) {
   res.status(404).send("Nothing there, 404.");
 })
 
-const port = 8000;
 app.listen(port, () => console.log('Example app listening on port ' + port + '!'));
