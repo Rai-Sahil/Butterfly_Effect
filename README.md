5a993cc0eccd90e0f993375ae4cac89a06e89376
Delson <delsonwtan@gmail.com>
2022-04-28 13:57:51 -0700

    Setup script for db initialization using mysql.

--- README.md
+++ README.md
@@ -9,7 +9,15 @@ Hi my name is Delson. I look forward to working on an organized group project wi
 Hi My name is Sahil Rai. I am really excited to work on databases specially mongoDB and showing my Frontend Skills. It'll be great working with my team, everyone seems chill. 
 
 
-## Setup
+## Packages
+- express
+- express-session
+- mysql2
+
+## Installing MySQL on a Device
+- Windows: visit https://dev.mysql.com/downloads/installer/ and download the installer, then follow the prompts to complete installation.
+
+## Project Setup
 1. Clone repository
 2. Navigate to project root
 3. Type `npm install` to install dependencies, then type `npm start` to start the app.
---
+++ init-db.js
@@ -0,0 +1,49 @@
+const mysql = require("mysql2/promise");
+
+const dbName = "bby32";
+
+// name | email | password
+const users = [
+  ["Daniel Shen", "danielshen@bby32.com", "danielshen"],
+  ["Delson Tan", "delsontan@bby32.com", "delsontan"],
+  ["Navdeep Litt", "navdeeplitt@bby32.com", "navdeeplitt"],
+  ["Sahil Rai", "sahilrai@bby32.com", "sahilrai"]
+]
+
+
+async function initDB() {
+  const connection = await mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "themark",
+    multipleStatements: true,
+  });
+
+  /**
+   * Create specified database if it does not exist, uses it.
+   * Then, creates user table if it does not exist.
+   */
+  const query = `
+    CREATE DATABASE IF NOT EXISTS ${dbName};
+    use ${dbName};
+    CREATE TABLE IF NOT EXISTS users (
+      ID int NOT NULL AUTO_INCREMENT,
+      name varchar(30),
+      email varchar(30),
+      password varchar(30),
+      PRIMARY KEY (ID)
+    );`;
+  await connection.query(query);
+
+  const [userRows] = await connection.query("SELECT * FROM users");
+
+  if (userRows.length == 0) {
+    const insertUsers = `INSERT INTO users (name, email, password) values ?`;
+    await connection.query(insertUsers, [users]);
+  }
+}
+
+initDB().then(()=> {
+    console.log("DB initiated.");
+    process.exit();
+});
