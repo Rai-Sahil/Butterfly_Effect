9b32a3e3e8dd0e5a5abb8b5d5ddd368aa14af5c6
Delson <delsonwtan@gmail.com>
2022-05-06 12:21:53 -0700

    Remove redundant code in DelayRedirect.

--- public/js/login.js
+++ public/js/login.js
@@ -8,18 +8,9 @@ function init() {
     }
 
     function DelayRedirect() {
-      var seconds = 1;
-      var dvCountDown = document.getElementById("dvCountDown");
-      var lblCount = document.getElementById("lblCount");
-      dvCountDown.style.display = "block";
-      lblCount.innerHTML = seconds;
       setTimeout(function () {
-        seconds--;
-        lblCount.innerHTML = seconds;
-        if (seconds == 0) {
-          dvCountDown.style.display = "none";
-          window.location.replace("/");
-        }
+        dvCountDown.style.display = "none";
+        window.location.replace("/");
       }, 1000);
     }
 
