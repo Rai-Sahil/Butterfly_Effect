ae22b845412cdd835f1df9b68075cf26bba3e5f0
kemp-liao <kempliao@gmail.com>
2022-05-18 11:13:21 -0700

    Created js file for the timeline.

---
+++ public/js/timeline.js
@@ -0,0 +1,29 @@
+function init() {
+    console.log("Client script loaded.");
+
+    function ajaxGET(path, callback) {
+        const xhr = new XMLHttpRequest();
+        xhr.onload = function () {
+          if (this.readyState == XMLHttpRequest.DONE && this.status == 200) {
+            callback(this.responseText);
+          }
+        };
+        xhr.open("GET", path);
+        xhr.send();
+      }
+
+      
+
+
+
+
+
+
+}
+
+document.onreadystatechange = () => {
+    if (document.readyState === "complete") {
+      console.info("Document fully loaded.");
+      init();
+    }
+  };
