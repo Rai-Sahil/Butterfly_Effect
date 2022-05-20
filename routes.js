b2baba3e1115ace5b3986d578497d405d709f848
Sahil Rai <raisahil580@gmail.com>
2022-05-20 12:17:58 -0700

    made Ending collection board; made 9 different ending scenarios/pages; routed the collection board to main page; changed login - sign up theme color;

--- routes.js
+++ routes.js
@@ -148,6 +148,68 @@ router.get("/aboutus", requireLoggedIn, function (req, res) {
   });
 });
 
+router.get("/Ending_Collection", requireLoggedIn, function (req, res) {
+  res.sendFile("Ending_Collection.html", {
+    root: __dirname + "/public/html",
+  });
+});
+
+
+
+router.get("/comfort-0_earth-0", requireLoggedIn, function (req, res) {
+  res.sendFile("Ending0-0.html", {
+    root: __dirname + "/public/html",
+  });
+});
+
+router.get("/comfort-0_earth-50", requireLoggedIn, function (req, res) {
+  res.sendFile("Ending0-50.html", {
+    root: __dirname + "/public/html",
+  });
+});
+
+router.get("/comfort-0_earth-100", requireLoggedIn, function (req, res) {
+  res.sendFile("Ending0-100.html", {
+    root: __dirname + "/public/html",
+  });
+});
+router.get("/comfort-50_earth-0", requireLoggedIn, function (req, res) {
+  res.sendFile("Ending50-0.html", {
+    root: __dirname + "/public/html",
+  });
+});
+
+router.get("/comfort-50_earth-50", requireLoggedIn, function (req, res) {
+  res.sendFile("Ending50-50.html", {
+    root: __dirname + "/public/html",
+  });
+});
+
+router.get("/comfort-50_earth-100", requireLoggedIn, function (req, res) {
+  res.sendFile("Ending50-100.html", {
+    root: __dirname + "/public/html",
+  });
+});
+
+router.get("/comfort-100_earth-0", requireLoggedIn, function (req, res) {
+  res.sendFile("Ending100-0.html", {
+    root: __dirname + "/public/html",
+  });
+});
+
+router.get("/comfort-100_earth-50", requireLoggedIn, function (req, res) {
+  res.sendFile("Ending100-50.html", {
+    root: __dirname + "/public/html",
+  });
+});
+
+router.get("/comfort-100_earth-100", requireLoggedIn, function (req, res) {
+  res.sendFile("Ending100-100.html", {
+    root: __dirname + "/public/html",
+  });
+});
+
+
 router.get(
   "/admin-dashboard",
   requireLoggedIn,
