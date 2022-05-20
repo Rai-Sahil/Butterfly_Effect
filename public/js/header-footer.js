04966c7fce78c7e1e172cd25d23f906c88620390
kemp-liao <kempliao@gmail.com>
2022-05-19 17:59:38 -0700

    Async error fixed.

--- public/js/header-footer.js
+++ public/js/header-footer.js
@@ -1,5 +1,16 @@
 "use strict";
 
+async function loadComponentToId(nodeId, component) {
+  await fetch(component)
+    .then(res => res.text())
+    .then(body => document.querySelector(nodeId).innerHTML = body)
+}
+
+async function loadHeaderFooter() {
+  await loadComponentToId("#header-placeholder", "../html/components/header.html");
+  await loadComponentToId("#footer-placeholder", "../html/components/footer.html");
+}
+
 
 function ajaxGET(path, callback) {
   const xhr = new XMLHttpRequest();
@@ -12,18 +23,9 @@ function ajaxGET(path, callback) {
   xhr.send();
 }
 
-ajaxGET("/checkadmin", function (isAdmin) {
-  if (JSON.parse(isAdmin)) {
+loadHeaderFooter().then(() => {ajaxGET("/checkadmin", async function (isAdmin) {
+  if (await JSON.parse(isAdmin)) {
     document.querySelector('#admin-dashboard').classList.toggle('display-none');
     document.querySelector('#game-editor').classList.toggle('display-none');
   }
-});
-
-function loadComponentToId(nodeId, component) {
-  fetch(component)
-    .then(res => res.text())
-    .then(body => document.querySelector(nodeId).innerHTML = body)
-}
-
-loadComponentToId("#header-placeholder", "../html/components/header.html");
-loadComponentToId("#footer-placeholder", "../html/components/footer.html");
+});});
