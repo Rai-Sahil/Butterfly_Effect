20b714a2d9a169029f4da8afa6e25dddcec1c504
kemp-liao <kempliao@gmail.com>
2022-05-24 17:26:12 -0700

    Allowed the ending-collection page to only show what user earned.

---
+++ public/js/ending-collection.js
@@ -0,0 +1,32 @@
+function init() {
+    console.info("Client script loaded.");
+    function ajaxGET(path, callback) {
+      const xhr = new XMLHttpRequest();
+      xhr.onload = function () {
+        if (this.readyState == XMLHttpRequest.DONE) {
+          callback(this.responseText, this.status);
+        }
+      };
+      xhr.open("GET", path);
+      xhr.send();
+    }
+
+    ajaxGET("/endings", (data) => {
+        const parsedData = JSON.parse(data);
+        const endings = parsedData.endings;
+        console.log(endings);
+        for (let i = 0; i < endings.length; i++){
+            document.getElementById("ending-" + endings[i].ending_id).classList.toggle("display-none");
+        }
+
+    });
+
+
+}
+
+document.onreadystatechange = () => {
+    if (document.readyState === "complete") {
+      console.info("Document fully loaded.");
+      init();
+    }
+  };
