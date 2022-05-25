050ec0826152e8f4d58dbacc959e8d56c1d5344d
kemp-liao <kempliao@gmail.com>
2022-05-24 18:11:10 -0700

    Fixed error.

--- public/js/ending-collection.js
+++ public/js/ending-collection.js
@@ -1,32 +1,33 @@
 function init() {
     console.info("Client script loaded.");
     function ajaxGET(path, callback) {
-      const xhr = new XMLHttpRequest();
-      xhr.onload = function () {
-        if (this.readyState == XMLHttpRequest.DONE) {
-          callback(this.responseText, this.status);
-        }
-      };
-      xhr.open("GET", path);
-      xhr.send();
+        const xhr = new XMLHttpRequest();
+        xhr.onload = function () {
+            if (this.readyState == XMLHttpRequest.DONE) {
+                callback(this.responseText, this.status);
+            }
+        };
+        xhr.open("GET", path);
+        xhr.send();
     }
 
     ajaxGET("/endings", (data) => {
         const parsedData = JSON.parse(data);
         const endings = parsedData.endings;
         console.log(endings);
-        for (let i = 0; i < endings.length; i++){
+        for (let i = 0; i < endings.length; i++) {
             document.getElementById("ending-" + endings[i].ending_id).classList.toggle("display-none");
         }
-
     });
+
+
 
 
 }
 
 document.onreadystatechange = () => {
     if (document.readyState === "complete") {
-      console.info("Document fully loaded.");
-      init();
+        console.info("Document fully loaded.");
+        init();
     }
-  };
+};
