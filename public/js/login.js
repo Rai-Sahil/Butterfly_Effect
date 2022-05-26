885769d57613f3b9d10d63532a754e35ffd825d2
Sahil Rai <raisahil580@gmail.com>
2022-05-25 21:32:59 -0700

    Giving Proper Names To The Images;

--- public/js/login.js
+++ public/js/login.js
@@ -96,7 +96,7 @@ document.addEventListener("readystatechange", () => {
 
 let image = document.getElementById("image");
 let image2 = document.getElementById("image2");
-let images = ["./img/blue.svg", "./img/blueflap.svg"];
+let images = ["./img/Butterfly_flaps_up.svg", "./img/Butterfly_flaps_down.svg"];
 setInterval(function () {
   let random = Math.floor(Math.random() * 2);
   image.src = images[random];
