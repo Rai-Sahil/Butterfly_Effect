7df0cb4a6e52fcdbbc8e36ed8e4e0bb3095edb0a
Wendy <mkong16@my.bcit.ca>
2022-05-25 17:34:59 -0700

    added ending senarios(texts) in the database table

--- constants.js
+++ constants.js
@@ -56,12 +56,12 @@ const choices = [
 
 // type | threshold | text
 const endings = [
-  ["comfort", 0, "this is comfort 0 ending"],
-  ["comfort", 50, "this is comfort 50 ending"],
-  ["comfort", 100, "this is comfort 100 ending"],
-  ["environment", 0, "this is environment 0 ending"],
-  ["environment", 50, "this is environment 50 ending"],
-  ["environment", 100, "this is environment 100 ending"],
+  ["comfort", 0, "You have sacrificed a lot to save earth and the world! As long as it does not lower your happiness too much, keep up the good work! You are the part of the solution."],
+  ["comfort", 50, "You reasonably compromised between your living comfort and care for earth. Remember. Small changes make a big difference. Look for more changes you can make for the better future."],
+  ["comfort", 100, "It is time to take action for climate change. Living comfort that you are enjoying right now might not be available within few years. To keep our happy life, we need to make some small changes in our daily life choices. Is there anything you can help to mitigate climate change?"],
+  ["environment", 0, "Earth is in danger, and human race is also in danger. Sea level has risen tremendously with increased acidity. 60% of all fish species have been wiped out. The Maldives, Kiribati, Tuvalu, Solomon Islands, and more countries have sunk in the ocean. More frequent wildfires, longer periods of drought, intense tropical storms are happening. People are exposed to extreme heat waves every 5 years and are in risk of injury, disease, and death."],
+  ["environment", 50, "It is not late to mitigate the climate change. Earth is still slowly going for the dead-end. Due to the research, we have about 10 years to cut emissions to avoid dire climate scenarios. Take the action now for our future."],
+  ["environment", 100, "Earth is breathing. Overall risks have been lowered. As 196 countries signed and promised on Paris Agreement, we have successfully avoided more dire impacts of climate change by limiting global warming to well below 2° C (3.6° F)—or even 1.5° C."],
 ];
 
 module.exports = {
